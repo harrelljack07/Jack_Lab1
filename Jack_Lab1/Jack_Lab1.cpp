1eed228b8ef4860c74455b6eb5f56c89bab0b19b
jharre16 <jharre16@CH108-05>
2026-04-13 12:11:46 -0500

    Accidental bad commit

--- Jack_Lab1/Jack_Lab1.cpp
+++ Jack_Lab1/Jack_Lab1.cpp
@@ -11,6 +11,7 @@ int main()
     cout << "Changes made by Fergiandane!" << endl;
     cout << "Lab 1 completed on April 8th 2026!" << endl;
     cout << "Feature by Jack: Branching is awesome!" << endl;
+    cout << "TYPO LINE!!" << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
